12c8f920a72c4ecc8ce60212aab67770565ae80c
alexr63 <alex@resnik.co.ua>
2014-01-03 10:54:35 +0200

    SelectedHotelsEntities extensions (not finished)

---
+++ SelectedHotelsEntities.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SelectedHotelsModel
+{
+    public partial class SelectedHotelsEntities
+   {
+        public IEnumerable<Hotel> HotelsInLocation(int locationId, int? hotelTypeId = null)
+        {
+            IList<Hotel> hotels = (from p in Products
+                                   where !p.IsDeleted
+                                   select p).OfType<Hotel>().ToList();
+            var query = from h in hotels
+                        where (h.LocationId == locationId || h.Location.ParentId == locationId ||
+                              (h.Location.ParentLocation != null && h.Location.ParentLocation.ParentId == locationId)) &&
+                              (hotelTypeId == null || h.HotelTypeId == hotelTypeId.Value)
+                        select h;
+            return query;
+        }
+    }
+}
